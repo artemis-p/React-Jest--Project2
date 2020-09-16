13a85cdac9575217e8fb114ce150a0ad9f0a0378
Artemis Papanikolaou <artemispapanik@gmail.com>
2020-09-16 13:59:30 +0100

    Added userEvent

--- tic-tac-toe-vanilla/src/NoteList.js
+++ tic-tac-toe-vanilla/src/NoteList.js
@@ -1,15 +1,39 @@
 import React, { Component } from "react";
 
 class NoteList extends Component{
+  constructor(props) {
+    super(props); {
+      this.state = {value: '', notes: []};
+      this.handleChange = this.handleChange.bind(this);
+
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event); {
+      this.setState({value: Event.target.value});
+    }
+
+    handleSubmit(event); {
+      this.state.notes.push(this.state.value)
+      this.setState({notes: this.state.notes});
+      event.preventDefault();
+    }
+  }
+
   render(){
     return(
       <div className="NoteList">
-        <form>
+        <form onSubmit={this.handleSubmit}>
           <label>
-            Add note: <input type="text" />
+            Add note: <input type="text" value={this.state.value} onChange={this.handleChange} />
           </label>
           <input type="submit" value="Submit" />
         </form>
+        {this.state.notes.map( (note, index) => {
+          return (
+            <p key={index}>{note}</p>
+          )
+        })}
       </div>
     )
   }
